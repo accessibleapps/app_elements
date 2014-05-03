69a5321c9c4f38529bbe08f4d02655dbf58ada14
Q <q@q-continuum.net>
2014-05-03 14:24:16 -0600

    If the system locale isn't one we support, set index to the default locale, as that's the one we're running in

--- app_elements/i18n.py
+++ app_elements/i18n.py
@@ -1,5 +1,6 @@
 import locale
 import i18n_core
+import babel
 
 from wx_utils import forms as wx_forms
 from gui_builder import fields
@@ -11,14 +12,20 @@ class LanguageSelectionPanel(wx_forms.AutoSizedPanel):
 
  def __init__(self, *args, **kwargs):
   super(LanguageSelectionPanel, self).__init__(*args, **kwargs)
-  self.locales = sorted(i18n_core.get_available_locales(application.name), key=lambda i: i.language, cmp=locale.strcoll)
+  locales = list(i18n_core.get_available_locales(application.name))
+  self.locales = sorted(locales, key=lambda i: i.language, cmp=locale.strcoll)
 
  def render(self, *args, **kwargs):
   super(LanguageSelectionPanel, self).render(*args, **kwargs)
   locales = [u"{name} ({english_name})".format(name=i.language_name, english_name=i.english_name) for i in self.locales]
   self.language.set_value(locales)
   current_locale = application.locale.split('.')[0]
-  self.language.set_index([i.language for i in self.locales].index(current_locale))
+  languages = [i.language for i in self.locales]
+  try:
+   index = languages.index(current_locale)
+  except ValueError:
+   index = languages.index(i18n_core.DEFAULT_LOCALE)
+  self.language.set_index(index)
 
  def set_config_values(self):
   locale = self.locales[self.language.get_index()]
