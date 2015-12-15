f9bf6d7e04af9f90023348ef9c2babf7fadb70cd
Q <q@q-continuum.net>
2015-12-14 20:43:21 -0700

    Bumped version to 0.3

--- app_elements/__init__.py
+++ app_elements/__init__.py
@@ -1,5 +1,5 @@
-__version__ = 0.23
-__doc__ = """Elements of a desktop application"""
+__version__ = 0.3
+__doc__ = """Reuseable elements of desktop applications"""
 __author__ = 'Christopher Toth <q@q-continuum.net>'
 
 def find_application_module():
