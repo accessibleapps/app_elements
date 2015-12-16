22d3774627eece2648e6984856607bb7ef62dfb9
Q <q@q-continuum.net>
2015-12-15 23:15:12 -0700

    Spaces to tabs

--- app_elements/__init__.py
+++ app_elements/__init__.py
@@ -3,5 +3,5 @@ __doc__ = """Reuseable elements of desktop applications"""
 __author__ = 'Christopher Toth <q@q-continuum.net>'
 
 def find_application_module():
- import application
- return application
+	import application
+	return application
